24385289c7f58cfd65ea926e3e52df762a0346e9
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 02:55:22 +0530

    adding LineFromUpView class

--- linefromupbarview/src/main/java/com/example/linefromupbarview/LineFromUpView.kt
+++ linefromupbarview/src/main/java/com/example/linefromupbarview/LineFromUpView.kt
@@ -54,3 +54,19 @@ fun Canvas.drawLFUNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawLineFromUp(scale, w, h, paint)
 }
+
+class LineFromUpView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
