28c771e763d2ce6d92f41121f39b367522a3d2b4
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 03:08:04 +0530

    adding static create method

--- linefromupbarview/src/main/java/com/example/linefromupbarview/LineFromUpView.kt
+++ linefromupbarview/src/main/java/com/example/linefromupbarview/LineFromUpView.kt
@@ -206,4 +206,13 @@ class LineFromUpView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : LineFromUpView {
+            val view : LineFromUpView = LineFromUpView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
