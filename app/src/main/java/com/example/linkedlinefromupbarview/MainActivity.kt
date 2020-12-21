43f9734b1eff90bc4eadbc941fdfd85e33c531da
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 03:11:22 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedlinefromupbarview/MainActivity.kt
+++ app/src/main/java/com/example/linkedlinefromupbarview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedlinefromupbarview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.linefromupbarview.LineFromUpView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LineFromUpView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
